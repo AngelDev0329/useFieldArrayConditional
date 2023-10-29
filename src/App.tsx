1e7c17be0e88503243f859b6ea0e008b19ba3dac
AngelDev0329 <rapanjel@hotmail.com>
2023-10-29 22:07:27 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  useForm,
+  useWatch,
+  useFieldArray,
+  Control,
+  UseFormRegister
+} from "react-hook-form";
+import "./styles.css";
+
+type FormValues = {
+  data: { name: string }[];
+};
+
+const ConditionField = ({
+  control,
+  index,
+  register
+}: {
+  control: Control<FormValues>;
+  index: number;
+  register: UseFormRegister<FormValues>;
+}) => {
+  const output = useWatch({
+    name: "data",
+    control
+  });
+
+  return (
+    <>
+      {/* Required shouldUnregister: false */}
+      {output[index]?.name === "bill" && (
+        <input {...register(`data.${index}.name` as const)} />
+      )}
+      {/* doesn't required shouldUnregister: false */}
+      <input
+        name={`data[${index}].easyConditional`}
+        style={{ display: output[index]?.name === "bill" ? "block" : "none" }}
+      />
+    </>
+  );
+};
+
+export default () => {
+  const { control, handleSubmit, register } = useForm<FormValues>({
+    defaultValues: {
+      data: [{ name: "test" }, { name: "test1" }, { name: "test2" }]
+    },
+    mode: "onSubmit"
+  });
+  const { fields } = useFieldArray({
+    control,
+    name: "data"
+  });
+
+  return (
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+      {fields.map((data, index) => (
+        <>
+          <input {...register(`data.${index}.name`)} />
+          <ConditionField register={register} control={control} index={index} />
+        </>
+      ))}
+      <input type="submit" />
+    </form>
+  );
+};
